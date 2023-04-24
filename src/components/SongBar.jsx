18777c8dbd2ef35ab891942b8a7bec0fc09e9c90
Anzanrai <rai.unknown@gmail.com>
2023-04-24 21:20:00 +1000

    TopArtists and TopCharts pages added and completed

--- src/components/SongBar.jsx
+++ src/components/SongBar.jsx
@@ -3,8 +3,12 @@ import { Link } from 'react-router-dom';
 
 import PlayPause from './PlayPause';
 
-const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
-  <div className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
+const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => 
+  {
+    console.log("Props song", song)
+    console.log("Props i", i)
+    console.log("Props artistId", artistId);
+  return <div className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
     <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
       <img
@@ -14,7 +18,7 @@ const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, h
       />
       <div className="flex-1 flex flex-col justify-center mx-3">
         {!artistId ? (
-          <Link to={`/songs/${song.key}`}>
+          <Link to={`/songs/${song?.id}`}>
             <p className="text-xl font-bold text-white">
               {song?.title}
             </p>
@@ -41,6 +45,6 @@ const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, h
       )
       : null}
   </div>
-);
+};
 
 export default SongBar;
