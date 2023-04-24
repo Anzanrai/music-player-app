64a2a3be26437e868b8385b6057e71336531ef52
Anzanrai <rai.unknown@gmail.com>
2023-04-24 21:18:44 +1000

    added new queries artistdetailquery and getsongsbycountry query and getsongrelated query

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -16,16 +16,19 @@ const options = {
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        baseUrl: 'https://shazam-core.p.rapidapi.com',
         prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key', '2db107413fmsh499182bf4fe9d9cp1ec41djsn920f0a43d0fa')
             return headers;
         }
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({query: () => '/charts/world'}),
-        getSongDetails: builder.query({query: ({songid}) => `/tracks/details?track_id=${songid}`})
+        getTopCharts: builder.query({query: () => '/v1/charts/world'}),
+        getSongDetails: builder.query({query: ({songid}) => `/v1/tracks/details?track_id=${songid}`}),
+        getSongRelated: builder.query({query: ({songid}) => `/v1/tracks/related?track_id=${songid}`}),
+        getArtistDetails: builder.query({query: (artistId) => `/v2/artists/details?artist_id=${artistId}`}),
+        getSongsByCountry: builder.query({query: (countryCode) => `/v1/charts/country?country_code=${countryCode}`}),
     }),
 });
 
-export const {useGetTopChartsQuery, useGetSongDetailsQuery} = shazamCoreApi;
+export const {useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery, useGetArtistDetailsQuery, useGetSongsByCountryQuery} = shazamCoreApi;
